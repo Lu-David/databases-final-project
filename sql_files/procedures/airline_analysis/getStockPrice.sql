b623f34b2946adc663da62bd545d78bd5701aff9
Tony Jung <tonyjung@tonys-air.acasmart.jh.edu>
2022-12-19 22:31:43 -0500

    Compiled procedures together and finalized changes to php

--- sql_files/procedures/airline_analysis/getStockPrice.sql
+++
@@ -1,9 +0,0 @@
--- Get Stock Price data for each airline 
-DROP PROCEDURE IF EXISTS getStockPrice
-DELIMITER |
-CREATE PROCEDURE getStockPrice(IN target_date DATE)
-    SELECT company, open, close
-    FROM stocks
-    WHERE date = target_date;
-|
-DELIMITER ;
